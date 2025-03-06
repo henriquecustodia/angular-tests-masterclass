73940161d7d362a8cacc72083abdeab2f60f695f
Henrique Custódia <martinshenrique8@gmail.com>
2025-03-06 09:09:25 -0300

    feat: cria component SearchBar

---
+++ src/app/components/search-bar/search-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-search-bar',
+  imports: [CommonModule],
+  templateUrl: './search-bar.component.html',
+  styleUrl: './search-bar.component.css',
+})
+export class SearchBarComponent {}
