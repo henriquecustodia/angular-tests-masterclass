bac83dad6a6960a669ad218ebe1bcfa6b2424177
Henrique Custódia <martinshenrique8@gmail.com>
2025-03-06 09:40:49 -0300

    feat: adiciona lógica para pesquisar filmes para SearchBarComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  imports: [SearchBarComponent],
 })
 export class AppComponent {
-  title = 'angular-tests-masterclass';
+  
+  onSearch($event: string) {
+    console.log($event);
+  }
+
 }
--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, output, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search-bar',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.css',
 })
-export class SearchBarComponent {}
+export class SearchBarComponent {
+  inputText = signal('');
+
+  searchText = output<string>();
+
+  onSearch() {
+    this.searchText.emit(this.inputText());
+    this.inputText.set('');
+  }
+}
