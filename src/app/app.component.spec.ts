90c659d662740521ca4306e9a51ca0c0bf88e867
Henrique Custódia <martinshenrique8@gmail.com>
2025-03-09 19:44:52 -0300

    feat: finaliza AppComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,126 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { Component, input, output, signal } from '@angular/core';
+import { Item, ListComponent } from './components/list/list.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { MovieModel, MoviesService } from './services/movies/movies.service';
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-search-bar',
+  template: '',
+})
+class FakeSearchBarComponent implements SearchBarComponent {
+  inputText = signal('');
+  searchText = output<string>();
+  onSearch(): void {
+    throw new Error('Method not implemented.');
+  }
+}
+
+@Component({
+  selector: 'app-list',
+  template: '',
+})
+class FakeListComponent implements ListComponent {
+  data = input.required<Item[]>();
+}
+
+class FakeMoviesService implements MoviesService {
+  httpClient!: HttpClient;
+
+  getMovies = jest.fn();
+  getMoviesByTitle = jest.fn();
+}
 
 describe('AppComponent', () => {
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    expect(fixture.componentInstance).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [AppComponent],
+      providers: [
+        {
+          provide: MoviesService,
+          useClass: FakeMoviesService,
+        },
+      ],
+    });
+
+    TestBed.overrideComponent(AppComponent, {
+      set: {
+        imports: [FakeListComponent, FakeSearchBarComponent],
+      },
+    });
   });
 
+  it('deve listar os filmes', () => {
+    const fakeMovies: MovieModel[] = [
+      { title: 'Filme 1' },
+      { title: 'Filme 2' },
+      { title: 'Filme 3' },
+    ];
+
+    const moviesService = TestBed.inject(MoviesService);
+
+    (moviesService.getMovies as jest.Mock).mockReturnValue(of(fakeMovies));
+
+    const fixture = TestBed.createComponent(AppComponent);
+
+    fixture.detectChanges();
+
+    expect(moviesService.getMovies).toHaveBeenCalled();
+
+    const listDebugEl = fixture.debugElement.query(
+      By.directive(FakeListComponent)
+    );
+
+    expect(listDebugEl.componentInstance.data()).toEqual(fakeMovies);
+  });
+
+  it('deve pesquisar um filme', () => {
+    const fakeSearchText = 'Kill';
+
+    const fakeKillBill = { title: 'Kill Bill' };
+    const fakeKillBill2 = { title: 'Kill Bill 2' };
+    const fakeTartarugasNinjas = { title: 'Tartarugas ninjas' };
+    const fakeMatrix = { title: 'Matrix' };
+
+    const fakeMovies = [
+      fakeKillBill,
+      fakeKillBill2,
+      fakeTartarugasNinjas,
+      fakeMatrix,
+    ];
+
+    const fakeFilteredMovies = [fakeKillBill, fakeKillBill2];
+
+    const moviesService = TestBed.inject(MoviesService);
+
+    (moviesService.getMovies as jest.Mock).mockReturnValue(of(fakeMovies));
+
+    (moviesService.getMoviesByTitle as jest.Mock).mockReturnValue(
+      of(fakeFilteredMovies)
+    );
+
+    const fixture = TestBed.createComponent(AppComponent);
+
+    fixture.detectChanges();
+
+    const searchBarDebugEl = fixture.debugElement.query(
+      By.directive(FakeSearchBarComponent)
+    );
+
+    searchBarDebugEl.componentInstance.searchText.emit(fakeSearchText);
+
+    fixture.detectChanges();
+
+    expect(moviesService.getMoviesByTitle).toHaveBeenCalledWith(fakeSearchText);
+
+    const listDebugEl = fixture.debugElement.query(
+      By.directive(FakeListComponent)
+    );
+
+    expect(listDebugEl.componentInstance.data()).toEqual(fakeFilteredMovies);
+  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { MovieModel, MoviesService } from './services/movies/movies.service';
+import { ListComponent } from './components/list/list.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [SearchBarComponent],
+  imports: [SearchBarComponent, ListComponent],
 })
-export class AppComponent {
-  
-  onSearch($event: string) {
-    console.log($event);
+export class AppComponent implements OnInit {
+  moviesService = inject(MoviesService);
+
+  movies = signal<MovieModel[]>([]);
+
+  ngOnInit() {
+    this.moviesService.getMovies().subscribe((movies) => {
+      this.movies.set(movies);
+    });
   }
 
+  onSearch(text: string) {
+    this.moviesService.getMoviesByTitle(text).subscribe((movies) => {
+      this.movies.set(movies);
+    });
+  }
 }
